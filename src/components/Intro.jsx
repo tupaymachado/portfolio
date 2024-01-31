5b9bb49e89320fb7426c737b248cec6ef62ad6cd
Tupay Machado <tupay.machado@gmail.com>
2024-01-31 17:57:27 -0300

    Reset CSS e componente title

---
+++ src/components/Intro.jsx
@@ -0,0 +1,21 @@
+import styles from './Intro.module.css';
+import { useState } from 'react';
+
+function Intro() {
+    return (
+        <>
+            <p>
+                Claro, aqui está uma versão mais curta e energizada da sua descrição pessoal:
+
+                Olá, Mundo Tech!
+
+                Eu sou um desenvolvedor web em ascensão, vibrante e apaixonado, mergulhado até o pescoço no meu segundo semestre de estudos. Minha especialidade? Um trio dinâmico de React, JavaScript e Firebase, com os quais estou moldando o futuro da web! Embora ainda não esteja oficialmente no campo como desenvolvedor, já deixei minha marca com dois projetos espetaculares no meu trabalho atual - sim, onde sou o super caixa!
+
+                Minha abordagem? Soluções simples, elegantes, mas poderosas! Estou sempre à procura de maneiras de transformar desafios complexos em soluções incríveis. E adivinha? Estou pronto para decolar na minha carreira tech! Estou de olho naquela vaga cobiçada de desenvolvedor júnior ou estagiário.
+
+                Quando não estou codificando, mergulho no mundo da cultura, dos games, filmes e música. Ah, e eu trago uma mistura única de Sociologia e Administração para a mesa, o que me dá uma perspectiva extraordinária sobre como a tecnologia pode tocar vidas.</p>
+        </>
+    )
+};
+
+export default Intro;
---
+++ src/components/Title.jsx
@@ -0,0 +1,14 @@
+import "@fontsource/dm-mono";
+import "@fontsource/dm-mono/500.css";
+import styles from './Title.module.css';
+
+function Title() {
+    return (
+        <>
+            <h1 className={styles.title}>Hello, World! I'm <span className={styles.name}>Tupay</span>.</h1>
+            <h2 className={styles.title}>I'm a Software Developer.</h2>
+        </>
+    );
+}
+
+export default Title;
