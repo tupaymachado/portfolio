df4179327f5bd577a4e9a8611216e894724d0f76
Tupay Machado <tupay.machado@gmail.com>
2024-02-12 17:57:06 -0300

    ajustes CSS

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,8 @@ import styles from './Menu.module.css';
 function Menu() {
     return (
         <nav className={styles.menu}>
-            <ul>
-                <li><a href="#intro">Intro</a></li>
+            <ul className={styles.menuList}>
+                <li><a href="#intro">Tupay</a></li>
                 <li><a href="#projects">Projects</a></li>
                 <li><a href="#contact">Contact</a></li>
                 <li><a href="#blog">Blog</a></li>
