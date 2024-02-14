6cf891bf5cfdab6aec79df5a5d7a507e9f8161bb
Tupay Machado <tupay.machado@gmail.com>
2024-02-14 17:54:29 -0300

    ajustes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,15 +1,21 @@
 import styles from './Menu.module.css';
+import { useState } from 'react';
 
 function Menu() {
+    const [showMenu, setShowMenu] = useState(false);
+
     return (
-        <nav className={styles.menu}>
-            <ul className={styles.menuList}>
-                <li><a href="#intro">Tupay</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#blog">Blog</a></li>
-            </ul>
-        </nav>
+        <>
+            <button onClick={() => setShowMenu(!showMenu)}></button>
+            <nav className={`${styles.menu} ${showMenu ? styles.show : styles.hide}`}>
+                <ul className={`${styles.menuList}`}>
+                    <li><a href="#intro">Tupay</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#contact">Contact</a></li>
+                    <li><a href="#blog">Blog</a></li>
+                </ul>
+            </nav>
+        </>
     );
 }
 
