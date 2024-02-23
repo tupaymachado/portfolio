5a10f2fb671f0490bf389df1e47be3ab5a2020b3
Tupay Machado <tupay.machado@gmail.com>
2024-02-23 18:00:26 -0300

    Ajustes

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,23 +5,21 @@ function Menu() {
     const [showMenu, setShowMenu] = useState(false);
 
     return (
-        <>
-            <div className={styles.navBar}>
-                <label className={`${styles.buttonsBurger} ${showMenu ? styles.menuActive : ''}`} htmlFor="burger" onClick={() => setShowMenu(!showMenu)}>
-                    <div className={styles.burgerLine}></div>
-                    <div className={styles.burgerLine}></div>
-                    <div className={styles.burgerLine}></div>
-                </label>
-            </div>
-            <nav className={`${styles.menu} ${showMenu ? styles.show : styles.hide}`}>
+        <nav className={styles.navBar}>
+            <label className={`${styles.buttonsBurger} ${showMenu ? styles.menuActive : ''}`} htmlFor="burger" onClick={() => setShowMenu(!showMenu)}>
+                <div className={styles.burgerLine}></div>
+                <div className={styles.burgerLine}></div>
+                <div className={styles.burgerLine}></div>
+            </label>
+            <div className={`${styles.menu} ${showMenu ? styles.show : " "}`}>
                 <ul className={`${styles.menuList}`}>
                     <li><a href="#intro">Tupay</a></li>
                     <li><a href="#projects">Projects</a></li>
                     <li><a href="#contact">Contact</a></li>
                     <li><a href="#blog">Blog</a></li>
                 </ul>
-            </nav>
-        </>
+            </div>
+        </nav>
     );
 }
 
